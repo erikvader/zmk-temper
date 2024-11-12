456430f0e20513a16983281d9ce8ae074e934cef
Erik Rimskog <erik.rimskog@gmail.com>
2024-11-12 16:13:32 +0100

    emacs dir-locals

---
+++ config/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((auto-mode-alist . (("\\.keymap\\'" . c-mode)))
+ (c-mode . ((eriks/disable-electric-indent-local . t))))
